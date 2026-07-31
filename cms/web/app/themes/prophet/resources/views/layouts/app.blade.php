01f7b441269c6d71d243a8cb8a8ec9d2a918f8a2
Essosolam Justin POTCHONA <j.potchona@thecodingmachine.com>
2026-07-31 15:53:14 +0000

    feat(theme): thème Sage installé, Tailwind retiré

---
+++ cms/web/app/themes/prophet/resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html @php(language_attributes())>
+  <head>
+    <meta charset="{{ get_bloginfo('charset') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @php(wp_head())
+  </head>
+  <body @php(body_class())>
+    @php(do_action('get_header'))
+    <main id="main">
+      @yield('content')
+    </main>
+    @php(do_action('get_footer'))
+    @php(wp_footer())
+  </body>
+</html>
